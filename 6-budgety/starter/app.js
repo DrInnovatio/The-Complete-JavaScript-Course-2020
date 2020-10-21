4bd2f8c9df59042dd1c9c36fefaa99856782893d
DrInnovatio <turing1723@gmail.com>
2020-10-21 17:30:52 +1000

    got this done, but still need to fix somethings.

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -172,6 +172,12 @@ var UIcontroller = (function () {
       return (type === 'exp' ? " - " : " + ") + " " + int + "." + dec;
     };
 
+      var nodeListForEach = function (list, callback) {
+        for (var i = 0; i < list.length; i++) {
+          callback(list[i], i);
+        }
+      };
+
   return {
     getInput: function () {
       return {
@@ -238,12 +244,6 @@ var UIcontroller = (function () {
     displayPercentages: function (percentages) {
       var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-      var nodeListForEach = function (list, callback) {
-        for (var i = 0; i < list.length; i++) {
-          callback(list[i], i);
-        }
-      };
-
       nodeListForEach(fields, function (current, index) {
         if (percentages[index] > 0) {
           current.textContent = percentages[index] + " % ";
@@ -261,7 +261,21 @@ var UIcontroller = (function () {
       months = ['January', 'February' , 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', ' December' ];
       month = now.getMonth();
       year = now.getFullYear();
-      document.querySelector(DOMstrings.dateLabel).textContent = months[month - 1] + ' ' + year;
+      document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+    },
+
+    changedType: function(){
+      var fields = document.querySelectorAll(
+        DOMstrings.inputType, ' , ' + 
+        DOMstrings.inputDescription + ' , ' + 
+        DOMstrings.inputValue
+      );
+
+      nodeListForEach(fields, function(cur){
+        cur.classList.toggle('red-focus');
+      });
+
+      document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
     },
 
     getDOMstrings: function () {
@@ -285,6 +299,8 @@ var controller = (function (budgetCtrl, UICtrl) {
       }
     });
     document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+    document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 
   };
 
